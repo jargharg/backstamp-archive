dab2f00b8689343ea2d82500d1c6343b0d461d18
Jarod Hargreaves <jarod.hargreaves@gmail.com>
2017-06-29 16:32:15 +0100

    Remove unnecessary semi-colons

--- js/backstamps.js
+++ js/backstamps.js
@@ -1,4 +1,4 @@
-var backstampCSV, bsImageArray,yearArray, $yearContainer, years, currentYear, filter;
+var backstampCSV, bsImageArray, yearArray, $yearContainer, years, currentYear, filter
 bsImageArray = [
 {year: 1985, filename: "1985_sponge", description: "Spongeware", type: "S"}, 
 {year: 1986, filename: "1986-1989_sponge", description: "Spongeware", type: "S"}, 
@@ -75,42 +75,43 @@ bsImageArray = [
 {year: 2016, filename: "2016_beardedtit", description: "Bearded Tit", type: "L"}, 
 {year: 2016, filename: "2016_sponge1", description: "Spongeware", type: "S"}, 
 {year: 2017, filename: "2017_sponge", description: "Spongeware", type: "S"}
-];
-yearArray = [];
-$yearContainer = $("#yearContainer");
+]
+yearArray = []
+$yearContainer = $("#yearContainer")
 
 // return input sorted with S first, L middle and X last - input array must be ordered by year
 function sortArray(arrayToSort) {
-	currentYear = arrayToSort[0].year;
-	var sortedArray = [];
-	var sLocation = -1;
-	var lLocation = 0;
-	var i=0;
+	currentYear = arrayToSort[0].year
+	var sortedArray = []
+	var sLocation = -1
+	var lLocation = 0
+	var i=0
+
 	while (i<arrayToSort.length){
 		if(arrayToSort[i].year === currentYear){
 			if (arrayToSort[i].type === "S") {
-				sortedArray.splice(sLocation+1,0,arrayToSort[i]);
-				sLocation++;
-				lLocation++;
+				sortedArray.splice(sLocation+1,0,arrayToSort[i])
+				sLocation++
+				lLocation++
 			} else if (arrayToSort[i].type === "L") {
-				sortedArray.splice(lLocation,0,arrayToSort[i]);
-				lLocation++;
+				sortedArray.splice(lLocation,0,arrayToSort[i])
+				lLocation++
 			} else {
-				sortedArray.splice(lLocation+10,0,arrayToSort[i]);
+				sortedArray.splice(lLocation+10,0,arrayToSort[i])
 			}
-			i++;
+			i++
 		} else { // updates currentYear and S/L locations if current array record is in a later year, then runs sorting again on the record
-			currentYear = arrayToSort[i].year;
-			sLocation = i-1;
-			lLocation = i;
+			currentYear = arrayToSort[i].year
+			sLocation = i-1
+			lLocation = i
 		}
 	}
-	return sortedArray;
+	return sortedArray
 }
 
 // return number of unique years in array
 function countYears(arrayToCount) {
-	return arrayToCount[arrayToCount.length-1].year - arrayToCount[0].year + 1;
+	return arrayToCount[arrayToCount.length-1].year - arrayToCount[0].year + 1
 }
 
 // create yearCells and imgCells and append to yearContainer
@@ -122,15 +123,15 @@ function createGrid(){
 			$("<text />")
 			.html(1985+i+"<i class='icon-right-marker icon-large'></i>")
 			.addClass("yearNum centreCell")
-		);
-		yearArray.push($newYearCell);
+			)
+		yearArray.push($newYearCell)
 
-		currentYear = 1985+i;
-		var currentYearBsArray = [];
+		currentYear = 1985+i
+		var currentYearBsArray = []
 
 		// !!! can this be optimised? e.g. set new start point from array, exit when new year
 		for (j = 0; j < bsImageArray.length; j++) { // puts all relevant images into current year backstamp array
-			if (bsImageArray[j].year == currentYear) currentYearBsArray.push(bsImageArray[j]);
+			if (bsImageArray[j].year == currentYear) currentYearBsArray.push(bsImageArray[j])
 		}
 
 		currentYearBsArray.forEach(function(bsCell){
@@ -148,31 +149,31 @@ function createGrid(){
 				.attr({
 					src: "https://www.emmabridgewater.co.uk/content/ebiz/eb/images/backstamps/s/"+bsCell.filename+"_s.jpg",
 					alt: bsCell.description
-				})
-			);
-			$newYearCell.push($newImgCell); // adds image cell to array with current year window at the start
-		});
+					})
+				)
+			$newYearCell.push($newImgCell) // adds image cell to array with current year window at the start
+		})
 	}
 
 	yearArray.forEach(function(year){
-		$yearContainer.append(year[0]); // adds first year window and then images after
+		$yearContainer.append(year[0]) // adds first year window and then images after
 		for (j = 1; j < year.length; j++) { // scans through images in that year
-			$yearContainer.append(year[j]); // adds first year window and then images after
-			year[j].click(clickImg(year[j]));
+			$yearContainer.append(year[j]) // adds first year window and then images after
+			year[j].click(clickImg(year[j]))
 		}
 	})
-	$yearContainer.after($("<div id = 'detailHeight'></div>")); // allows us to get height of detailContainerE
+	$yearContainer.after($("<div id = 'detailHeight'></div>")) // allows us to get height of detailContainerE
 }
 
 // load detailContainers and spacerCells
 function addDetailContainers(){
-	$(".detailContainer").remove();
-	$(".spacerCell").parent().remove();
+	$(".detailContainer").remove()
+	$(".spacerCell").parent().remove()
 
-	$(".activeBsImg").removeClass("activeBsImg");
-	$gridCells = $(".gridCell");
+	$(".activeBsImg").removeClass("activeBsImg")
+	$gridCells = $(".gridCell")
 	for(i=0; i<$gridCells.length; i++) {
-		var $currentCell = $($gridCells[i]);
+		var $currentCell = $($gridCells[i])
 		if($yearContainer.offset().left+$yearContainer.width()-$currentCell.offset().left-$currentCell.width() < 30 || i === $gridCells.length-1){
 			if($currentCell.hasClass("visYearCell")) {
 				$currentCell.before(
@@ -191,22 +192,22 @@ function addDetailContainers(){
 		}
 	}
 	// square cells
-	$(".gridCell").height($(".gridCell").width());
+	$(".gridCell").height($(".gridCell").width())
 }
 
 // select next or previous imgCell based on input "L" or "R"
 function navigateCells(direction){
-	var $parentCell = $(".activeBsImg").parent();
-	(direction === "L") ? $parentCell.prevAll(".imgCell:visible").first().click() : $parentCell.nextAll(".imgCell:visible").first().click();
+	var $parentCell = $(".activeBsImg").parent()
+	(direction === "L") ? $parentCell.prevAll(".imgCell:visible").first().click() : $parentCell.nextAll(".imgCell:visible").first().click()
 }
 
 // navigate cells using arrow keys
 function addKBNavigation(){
 	$(document).keydown(function(key){
 		if(key.which == 37) {
-			navigateCells("L");
+			navigateCells("L")
 		} else if (key.which == 39) {
-			navigateCells("R");
+			navigateCells("R")
 		}
 	})
 }
@@ -214,52 +215,52 @@ function addKBNavigation(){
 // handle use of top filters
 function loadOptions(){
 	$(".filterOption").click(function(){
-		$selectedFilter = $(this);
+		$selectedFilter = $(this)
 
 		if ($selectedFilter.hasClass("filterActive")) { // if active filter button has been clicked on, remove filters
-			filter = null;
-			$selectedFilter.removeClass("filterActive");
-			$("#viewAllButton").addClass("filterActive");
+			filter = null
+			$selectedFilter.removeClass("filterActive")
+			$("#viewAllButton").addClass("filterActive")
 		} else { // checks which filter button has been clicked on
-			filter = $selectedFilter.attr("data-filterType"); 
-			$(".filterActive").removeClass("filterActive");
-			$selectedFilter.addClass("filterActive");
+			filter = $selectedFilter.attr("data-filterType")
+			$(".filterActive").removeClass("filterActive")
+			$selectedFilter.addClass("filterActive")
 		}
 
 		// fades out yearContainer while cells are shown/hidden
 		$("#yearContainer").css("opacity",0).delay(110).queue(function(next){
 			if (filter) { // if a filter has been selected, checks if imgCell matches the filter type; displays cell if so, hides cell if not
-				$(".imgCell[data-bsType="+filter+"]").attr("data-display",1);
-				$(".imgCell[data-bsType!="+filter+"]").hide().attr("data-display",0);
+				$(".imgCell[data-bsType="+filter+"]").attr("data-display",1)
+				$(".imgCell[data-bsType!="+filter+"]").hide().attr("data-display",0)
 			} else { // if view all clicked, set all cells to be displayed
-				$(".imgCell").attr("data-display",1);
+				$(".imgCell").attr("data-display",1)
 			}
-			reloadGrid();
-			$("#yearContainer").css("opacity",1);
-			next();
-		});
+			reloadGrid()
+			$("#yearContainer").css("opacity",1)
+			next()
+		})
 	})
 }
 
 // show/hide imgCells of selected year based on filter
 function loadImgs(yearToDisplay) {
-	$firstWindow = $(yearToDisplay[0]);
-	$firstWindow.children().html($firstWindow.children().html().substring(0,4));
-	var validImages = 0;
+	$firstWindow = $(yearToDisplay[0])
+	$firstWindow.children().html($firstWindow.children().html().substring(0,4))
+	var validImages = 0
 
 	for (j = 1; j < yearToDisplay.length; j++) {
-		$imgDisplay = yearToDisplay[j];
+		$imgDisplay = yearToDisplay[j]
 		if($imgDisplay.attr("data-display") == 1) { // if filter is set to show the filtered backstamp type, proceed
-			$imgDisplay.show();
-			validImages++;
+			$imgDisplay.show()
+			validImages++
 		}
 	}
 
 	if (validImages === 0) {
-		$firstWindow.removeClass("visYearCell");
+		$firstWindow.removeClass("visYearCell")
 	} else {
-		$firstWindow.addClass("visYearCell");
-		$firstWindow.children().html($firstWindow.children().html()+"<i class='icon-right-marker icon-large'></i>");
+		$firstWindow.addClass("visYearCell")
+		$firstWindow.children().html($firstWindow.children().html()+"<i class='icon-right-marker icon-large'></i>")
 	}
 }
 
@@ -267,14 +268,14 @@ function loadImgs(yearToDisplay) {
 function clickImg(img){
 	return function(){
 		// find next detailContainer
-		var $nextDetailContainer = $(this).nextAll(".detailContainer").first();
+		var $nextDetailContainer = $(this).nextAll(".detailContainer").first()
 
 		if($(this).children().hasClass("activeBsImg")) {
 			// if clicking on currently selected cell
-			hideDetails();
+			hideDetails()
 		} else {
-			var prevBsImgTop = $(".activeBsImg")[0] ? $(".activeBsImg").offset().top :  null;
-			hideDetails();
+			var prevBsImgTop = $(".activeBsImg")[0] ? $(".activeBsImg").offset().top :  null
+			hideDetails()
 
 			// create content for detailContainer
 			$bsImgDetail = $("<div />")
@@ -282,7 +283,7 @@ function clickImg(img){
 			.append(
 				$("<img>")
 				.attr("src","https://www.emmabridgewater.co.uk/content/ebiz/eb/images/backstamps/"+$(this).attr("data-img")+".jpg")
-			);
+				)
 
 			$bsDataDetail = $("<div />")
 			.addClass("bsDataDetail")
@@ -290,36 +291,36 @@ function clickImg(img){
 				$("<div />")
 				.addClass("bsYear")
 				.html($(this).attr("data-img").substring(0,4))
-			)
+				)
 			.append(
 				$("<div />")
 				.addClass("bsName")
 				.html($(this).attr("data-desc"))
-			);
+				)
 
 			// expand next detailContainer and append content
 			$(this).nextAll(".detailContainer").first().addClass("detailContainerE")
 			.append(addNavigation())
 			.append($bsDataDetail.hide().fadeIn(200))
-			.append($bsImgDetail.hide().fadeIn(200));
+			.append($bsImgDetail.hide().fadeIn(200))
 
 			// re-arrange details for mobile screens
-			if ($(window).width() < 600) $bsImgDetail.insertBefore($bsDataDetail);
+			if ($(window).width() < 600) $bsImgDetail.insertBefore($bsDataDetail)
 
-			$(this).children().addClass("activeBsImg");
+			$(this).children().addClass("activeBsImg")
 
 			// scroll to top of selected cell
-			var currentBsImgTop = $(this).children().offset().top-8;
-			var scrollPoint = (prevBsImgTop > currentBsImgTop || prevBsImgTop === null) ? currentBsImgTop : currentBsImgTop - $("#detailHeight").height();
-			$('html, body').clearQueue().animate({scrollTop: scrollPoint}, 200, 'swing');
+			var currentBsImgTop = $(this).children().offset().top-8
+			var scrollPoint = (prevBsImgTop > currentBsImgTop || prevBsImgTop === null) ? currentBsImgTop : currentBsImgTop - $("#detailHeight").height()
+			$('html, body').clearQueue().animate({scrollTop: scrollPoint}, 200, 'swing')
 		}
 	}
 }
 
 // hide expanded detailContainer and un-highlights active img
 function hideDetails(){
-	$(".detailContainerE").removeClass("detailContainerE").empty();
-	$(".activeBsImg").removeClass("activeBsImg");
+	$(".detailContainerE").removeClass("detailContainerE").empty()
+	$(".activeBsImg").removeClass("activeBsImg")
 }
 
 // add navigation buttons to detailContainer
@@ -344,38 +345,38 @@ function addNavigation(){
 		.html("<i class='icon-close icon-large'></i>")
 		.click(hideDetails)
 		)
-	return $newNav;
+	return $newNav
 }
 
 // re-draw cells and detailContainers
 function reloadGrid(){
-	yearArray.forEach(function(year){ loadImgs(year) });
-	addDetailContainers();
-	$(".spacerCell").css("padding",0);
+	yearArray.forEach(function(year){ loadImgs(year) })
+	addDetailContainers()
+	$(".spacerCell").css("padding",0)
 }
 
 // Re-draw yearCells in IE to fix alignment issues
 function IERefresh(){ 
-	if (navigator.appVersion.indexOf("MSIE") > 0 || navigator.appVersion.indexOf("Trident") > 0) reloadGrid();
+	if (navigator.appVersion.indexOf("MSIE") > 0 || navigator.appVersion.indexOf("Trident") > 0) reloadGrid()
 }
 
 
 function initialiseBSApp(){
-	bsImageArray = sortArray(bsImageArray);
-	years = countYears(bsImageArray);
-	createGrid();
-	loadOptions();
-	$("button[data-filterType='S']").click();
-	addDetailContainers();
-	addKBNavigation();
-	navigateCells();
-	IERefresh();
+	bsImageArray = sortArray(bsImageArray)
+	years = countYears(bsImageArray)
+	createGrid()
+	loadOptions()
+	$("button[data-filterType='S']").click()
+	addDetailContainers()
+	addKBNavigation()
+	navigateCells()
+	IERefresh()
 }
 
 $(document).ready(function(){
-	initialiseBSApp();
-});
+	initialiseBSApp()
+})
 
 $(window).resize(function(){
-	reloadGrid();
-});
+	reloadGrid()
+})
